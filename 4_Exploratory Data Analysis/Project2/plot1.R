3d231bea8a9e4762f0cbdad10ca493fd68ad533c
Naqeeb Asif <naqeebasif@gmail.com>
2018-01-31 16:42:28 +0000

    ExAnalysis Project 2

---
+++ 4_Exploratory Data Analysis/Project2/plot1.R
@@ -0,0 +1,62 @@
+##-------------------------------------------Project2---------------------------
+
+##Plot1.R
+
+##------------------------------Loading the data--------------------------------
+# downloading the source file
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip",
+              destfile = "./FNEI_data.zip")
+
+#Unzipping the source file
+unzip("./FNEI_data.zip")
+
+#Loading the data
+NEI <- readRDS("./summarySCC_PM25.rds")
+SCC <- readRDS("./Source_Classification_Code.rds")
+
+##------------------------------------------------------------------------------
+
+##-------------------------------Checking the summary of the data---------------
+#Checking the structure of the data
+str(NEI)
+str(SCC)
+
+##Checking the summary of the data
+summary(NEI)
+summary(SCC)
+
+##-----------------------------------------------------------------------------
+
+##----------------------------Total emission per year--------------------------
+library(dplyr)
+total_emission_year <- NEI %>% select(year,Emissions) %>%group_by(year) %>% 
+                        summarise(total_emissions = sum(Emissions))
+
+##-----------------------------------------------------------------------------
+
+##-----------------------------------------------------------------------------
+###-------------------------Total Eimssion Plot---------------------------------
+
+with(total_emission_year,plot(year,total_emissions,xlab = "Year",
+                              ylab = "Total Emsissions", 
+                              main="Total Emissions Per year in United States",
+                              pch=19))
+
+with(total_emission_year,lines(year,total_emissions))
+
+dev.copy(png,"plot1.png")
+
+dev.off()
+
+#-------------------------------------------------------------------------------
+##Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?"
+
+if((total_emission_year[total_emission_year$year==2008,]$total_emissions) < (total_emission_year[total_emission_year$year==1999,]$total_emissions)){
+  bool_dec<-"Yes"
+}else {
+ bool_dec<-"No" 
+}
+
+cat("Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?",
+    bool_dec)
+#---------------------------------------------------------------------------------
